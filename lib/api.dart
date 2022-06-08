4ed8f1f81269eaa287fd0fd888cd6feca22fbcc5
Ahmad Malik <arma7x@live.com>
2022-06-08 18:42:25 +0800

    refactor

--- lib/api.dart
+++ lib/api.dart
@@ -1,28 +1,32 @@
+import 'dart:io';
 import 'dart:core';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:flutter/material.dart' show BuildContext;
-import 'package:flutter_laravel/bloc/AuthState.dart';
+import 'package:flutter/material.dart' show BuildContext, ScaffoldMessenger, SnackBar, Text;
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_laravel/bloc/AuthState.dart';
+import 'package:flutter_laravel/bloc/UserState.dart';
 
 class Api {
 
-    static const String BASE_URL = '192.168.56.1:8000';
-    static const String PING = 'api/ping';
-    static const String CREATE_TOKEN = 'api/tokens/create';
-    static const String USER_INFO = 'api/user';
-    static const String LOGIN = 'login';
-    static const String REGISTER = 'register';
-    static const String RESET_PASSWORD = 'password/reset';
+    static const String tokenKey = "sanctum-token";
+    static const String userKey = "laravel-user";
+    static const String baseUrl = '192.168.56.1:8000';
+    static const String pingPath = 'api/pingPath';
+    static const String createTokenPath = 'api/tokens/create';
+    static const String userInfoPath = 'api/user';
+    static const String loginPath = 'loginPath';
+    static const String registerPath = 'registerPath';
+    static const String resetPasswordPath = 'password/reset';
 
-    static Future<http.Response> PingServer() async {
-        final url = Uri.http(BASE_URL, PING);
+    static Future<http.Response> pingServer() async {
+        final url = Uri.http(baseUrl, pingPath);
         return await http.get(url);
     }
 
-    static Future<http.Response> CreateToken(Map<String, String> parameter) async {
-        final url = Uri.http(BASE_URL, CREATE_TOKEN);
+    static Future<http.Response> createToken(Map<String, String> parameter) async {
+        final url = Uri.http(baseUrl, createTokenPath);
         return await http.post(
             url,
             headers: <String, String>{
@@ -32,77 +36,104 @@ class Api {
         );
     }
 
-    static Future<http.Response> GetUserInfo(String token) async {
-        final url = Uri.http(BASE_URL, CREATE_TOKEN);
+    static Future<http.Response> getUserInfo(String token) async {
+        final url = Uri.http(baseUrl, userInfoPath);
         return await http.get(
             url,
             headers: <String, String>{
                 'Content-Type': 'application/json; charset=UTF-8',
-                'Authorization': 'Bearer ${token}',
+                'Authorization': 'Bearer $token',
             }
         );
     }
 
-    static String GetLoginLink() {
-        return Uri.http(BASE_URL, LOGIN).toString();
+    static String getLoginLink() {
+        return Uri.http(baseUrl, loginPath).toString();
     }
 
-    static String GetRegisterLink() {
-        return Uri.http(BASE_URL, REGISTER).toString();
+    static String getRegisterLink() {
+        return Uri.http(baseUrl, registerPath).toString();
     }
 
-    static String GetResetPasswordLink() {
-        return Uri.http(BASE_URL, RESET_PASSWORD).toString();
+    static String getResetPasswordLink() {
+        return Uri.http(baseUrl, resetPasswordPath).toString();
     }
 
-    static void ValidateToken(BuildContext context) async {
-        final _storage = const FlutterSecureStorage();
-        String? token = await _storage.read(key: "sanctum-token");
+    static IOSOptions _getIOSOptions() {
+        return const IOSOptions(accountName: "flutter-laravel");
+    }
+
+    static AndroidOptions _getAndroidOptions() {
+        return const AndroidOptions(encryptedSharedPreferences: true);
+    }
+
+    static void validateToken(BuildContext context) async {
+        const storage = FlutterSecureStorage();
+        String? token = await storage.read(key: tokenKey); // '78|EyFHNquvDBLxsD7GS9YudY5nfhJyKBMuFMIfBKcb';
+        // Map<String, dynamic> userMap = json.decode(user!);
         if (token != null) {
-            print('ValidateToken: ${token}');
-            SaveToken(token, context);
-            // GetUserInfo(token);
-            // if connectionless > retrieve UserState from cache
-            // if 401 > RemoveToken(context) && UserState from cache
-            // else > update UserState cached
+            String? user = await storage.read(key: userKey);
+            try {
+                final response = await getUserInfo(token);
+                print(response.body);
+                final responseBody = json.decode(response.body);
+                if (response.statusCode == 200) {
+                    print(responseBody); // save
+                } else if (response.statusCode == 401) {
+                    final snackBar = SnackBar(content: Text(responseBody['message']!));
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    destroySession(context);
+                } else {
+                    final snackBar = SnackBar(content: Text("Unknown"));
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                }
+            } on SocketException {
+                final snackBar = SnackBar(content: Text("No Internet connection 😑"));
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            } on HttpException {
+                final snackBar = SnackBar(content: Text("Couldn't find the post 😱"));
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            } on FormatException {
+                final snackBar = SnackBar(content: Text("Bad response format 👎"));
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            }
         } else {
-            print('ValidateToken bad');
-            RemoveToken(context);
+            print("Please login");
         }
     }
 
-    static void SaveToken(String token, BuildContext context) async {
-        try {
-            final _storage = const FlutterSecureStorage();
-            await _storage.write(
-                key: "sanctum-token",
-                value: token,
-                iOptions: const IOSOptions(accountName: "flutter-laravel"),
-                aOptions: const AndroidOptions(encryptedSharedPreferences: true),
-            );
-            BlocProvider.of<AuthState>(context).add(UserLoggedIn());
-            String? value = await _storage.read(key: "sanctum-token");
-            // return Future<bool>.value(true);
-        } catch(e) {
-            RemoveToken(context);
-            // return Future<bool>.value(false);
-        }
+    static void saveSession(String token, Map<String, dynamic> user, BuildContext context) async {
+        const storage = FlutterSecureStorage();
+        await storage.write(
+            key: tokenKey,
+            value: token,
+            iOptions: _getIOSOptions(),
+            aOptions: _getAndroidOptions(),
+        );
+        BlocProvider.of<AuthState>(context).add(UserLoggedIn());
+        await storage.write(
+            key: userKey,
+            value: json.encode(user),
+            iOptions: _getIOSOptions(),
+            aOptions: _getAndroidOptions(),
+        );
+        BlocProvider.of<UserState>(context).saveUser(user);
     }
 
-    static void RemoveToken(BuildContext context) async {
-        final _storage = const FlutterSecureStorage();
-        await _storage.delete(
-            key: "sanctum-token",
-            iOptions: const IOSOptions(accountName: "flutter-laravel"),
-            aOptions: const AndroidOptions(encryptedSharedPreferences: true),
+    static void destroySession(BuildContext context) async {
+        const storage = FlutterSecureStorage();
+        await storage.delete(
+            key: tokenKey,
+            iOptions: _getIOSOptions(),
+            aOptions: _getAndroidOptions(),
         );
         BlocProvider.of<AuthState>(context).add(UserLoggedOut());
+        await storage.delete(
+            key: userKey,
+            iOptions: _getIOSOptions(),
+            aOptions: _getAndroidOptions(),
+        );
+        BlocProvider.of<UserState>(context).removeUser();
     }
-
-    // save token from secure storage
-    static SaveUserInfo(Map<String, dynamic> info) {}
-
-    // remove token from secure storage
-    static RemoveUserInfo() {}
 
 }
